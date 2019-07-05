39cb733e6927285e4b6fd218bfaefc793a00075e
Gonzalo Alejandro Saad <gonzalo.saad@wnco.com>
2019-07-05 15:33:27 -0500

    Adding dynamic SQS local stack port

--- src/main/java/com/swa/sqs/Environment.java
+++ src/main/java/com/swa/sqs/Environment.java
@@ -4,17 +4,23 @@ import java.util.Optional;
 
 public class Environment {
 
-    private static final String DEFAuLT_ENDPOINT = "http://localhost:4576/";
+    private static final String DEFAULT_ENDPOINT_TEMPLATE = "http://localhost:%s/";
     private static final String DEFAULT_REGION = "us-east-1";
+    private static final String DEFAULT_PORT = "4576";
 
 
     public static String getBaseEndpoint(){
         return Optional.ofNullable(System.getProperty("SQS_ENDPOINT"))
-                .orElse(DEFAuLT_ENDPOINT);
+                .orElse(String.format(DEFAULT_ENDPOINT_TEMPLATE, getSQSPort()));
     }
 
     public static String getRegion(){
         return Optional.ofNullable(System.getProperty("SQS_REGION"))
                 .orElse(DEFAULT_REGION);
+    }
+
+    public static String getSQSPort(){
+        return Optional.ofNullable(System.getProperty("SQS_PORT"))
+                .orElse(DEFAULT_PORT);
     }
 }
